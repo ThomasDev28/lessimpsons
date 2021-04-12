190b3ae675adf6018a43ffe93a1d23dc3a131e30
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-04-12 15:04:42 +0200

    Finis api simpson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import QuoteCard from './components/QuoteCard';
+
+
+const defaultSimpsons = {quote:"Ahh! Sweet liquor eases the pain.",
+character:"Troy McClure",
+image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FTroyMcClure.png?1497567511112",
+characterDirection:"Right"};
+ 
 
 function App() {
+  const [MontreSimpson, setMontreSimpson] = React.useState(defaultSimpsons);
+    const mySimpsons = () => {
+    
+      axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
+     .then((response) => response.data)
+    
+        .then((data) => {
+          console.log(data)
+    
+          setMontreSimpson(data[0]);
+    
+        });
+    
+    };
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <QuoteCard {...MontreSimpson} />
+     <button onClick= {mySimpsons}>Famille Simpsons uniquement</button>
     </div>
   );
 }
 
 export default App;
+
